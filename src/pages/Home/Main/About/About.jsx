30aba123cb7b4f6c02282750d780228e89463fc7
Samuel-Sales <cacabrbf@hotmail.com>
2024-01-25 15:13:42 -0300

    Add more icons

--- src/pages/Home/Main/About/About.jsx
+++ src/pages/Home/Main/About/About.jsx
@@ -3,6 +3,9 @@ import { FaReact } from "react-icons/fa";
 import { SiNextdotjs } from "react-icons/si";
 import { FaNode } from "react-icons/fa";
 import { BiLogoPostgresql } from "react-icons/bi";
+import { FaJava } from "react-icons/fa6";
+import { FaFigma } from "react-icons/fa6";
+import { FaGithub } from "react-icons/fa";
 
 export default function About() {
     return (
@@ -15,10 +18,13 @@ export default function About() {
                 malesuada
             </p>
             <div>
-                <p><FaReact size={50} /></p>
+                <p><FaReact size={50} color='#5ebbf1' /></p>
                 <p><SiNextdotjs size={50} /></p>
-                <p><FaNode size={50} /></p>
-                <p><BiLogoPostgresql size={50} /></p>
+                <p><FaNode size={50} color='green' /></p>
+                <p><BiLogoPostgresql size={50}  color='blue' /></p>
+                <p><FaJava size={50} color='red' /></p>
+                <p><FaGithub size={50} /></p>
+                <p><FaFigma size={50} /></p>
             </div>
         </section>
     );
